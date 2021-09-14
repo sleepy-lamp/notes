e15b46c41bbfcbaad4a60c2206a25dc808546012
zlh <1903213068@qq.com>
2021-09-15 00:47:26 +0800

    优化

--- examples/1-reactphp-http-server.php
+++ examples/1-reactphp-http-server.php
@@ -6,39 +6,33 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $http = new React\Http\HttpServer(function (Psr\Http\Message\ServerRequestInterface $request) {
 
-
-    $factory = new React\MySQL\Factory();
-    $connection = $factory->createLazyConnection('root:123456@localhost/demo-zlh');
-
-    $stream = new \React\Stream\ThroughStream();
-
-    $connection->query('select sleep(1), now()')->then(
-        function (QueryResult $command) {
-            return $command->resultRows;
-        }
-    )->then(function($row0) use ($connection) {
-        return $connection->query('select sleep(1), now()')->then(function (QueryResult $command) use($row0) {
-             return array_merge($row0, $command->resultRows);
-        });
-    })->then(function ($result) use ($connection, $stream) {
-        $stream->end(var_export($result, true));
-        $connection->quit();
-    });
-
-
-
     foreach ($request->getHeaders() as $key => $item) {
         echo $key, ': ', implode(';', $item), PHP_EOL;
     }
     echo PHP_EOL;
 
-    return new React\Http\Message\Response(
-        200,
-        array(
-            'Content-Type' => 'text/plain'
-        ),
-        $stream
-    );
+    $factory = new React\MySQL\Factory();
+    $connection = $factory->createLazyConnection('root:123456@localhost/demo');
+
+    return $connection->query('select sleep(1), now()')->then(
+        function (QueryResult $command) {
+            return $command->resultRows;
+        }
+    )->then(function ($row0) use ($connection) {
+        return $connection->query('select sleep(1), now()')->then(function (QueryResult $command) use ($row0) {
+            return array_merge($row0, $command->resultRows);
+        });
+    })->then(function ($result) use ($connection) {
+        $connection->quit();
+        return var_export($result, true);
+    })->then(function ($export) {
+        return $response = new React\Http\Message\Response(
+            200,
+            ['Content-Type' => 'text/plain'],
+            $export
+        );
+    });
+
 });
 
 $socket = new React\Socket\SocketServer('0.0.0.0:8100');
